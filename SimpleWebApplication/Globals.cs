64ffccb579fd0a71fb24e77353bb70cd66400e62
fschwiet <fschwiet@gmail.com>
2011-12-20 20:18:28 -0800

    adding application id meta tag

---
+++ SimpleWebApplication/Globals.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleWebApplication
+{
+    public class Globals
+    {
+        public static string FacebookApplicationId
+        {
+            get { return Properties.Settings.Default.FacebookApplicationId; }
+        }
+    }
+}
