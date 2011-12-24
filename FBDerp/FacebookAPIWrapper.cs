31ff43a1806f932ab626cd909d1357f93c0c1d66
fschwiet <fschwiet@gmail.com>
2011-12-24 08:21:40 -0800

    extract downloadstring function

--- FBDerp/FacebookAPIWrapper.cs
+++ FBDerp/FacebookAPIWrapper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -12,24 +13,21 @@ namespace FBDerp
     {
         public static string GetAppToken(string applicationId, string applicationSecret)
         {
-            using(var client = new WebClient())
-            {
-                QuerystringParameters parameters = new QuerystringParameters();
+            QuerystringParameters parameters = new QuerystringParameters();
 
-                parameters.Add("client_id", applicationId);
-                parameters.Add("client_secret", applicationSecret);
-                parameters.Add("grant_type", "client_credentials");
+            parameters.Add("client_id", applicationId);
+            parameters.Add("client_secret", applicationSecret);
+            parameters.Add("grant_type", "client_credentials");
 
-                var uri = "https://graph.facebook.com/oauth/access_token?" + parameters.AsQuerystring();
+            var uri = "https://graph.facebook.com/oauth/access_token?" + parameters.AsQuerystring();
 
-                var result = client.DownloadString(uri);
+            var result = DownloadString(uri);
 
-                string prefix = "access_token=";
-                if (!result.StartsWith(prefix))
-                    throw new Exception("Unexpected response creating access token - expected prefix 'access_token=', actually started with " + result.Take(20));
+            string prefix = "access_token=";
+            if (!result.StartsWith(prefix))
+                throw new Exception("Unexpected response creating access token - expected prefix 'access_token=', actually started with " + result.Take(20));
 
-                return result.Substring(prefix.Length);
-            }
+            return result.Substring(prefix.Length);
         }
 
         public class APITestUser
@@ -43,23 +41,20 @@ namespace FBDerp
 
         public static APITestUser CreateUser(string accessToken, string applicationId, string userFullname)
         {
-            using (var webClient = new WebClient())
-            {
-                var parameters = new QuerystringParameters();
+            var parameters = new QuerystringParameters();
 
-                parameters.Add("installed", "true");
-                parameters.Add("name", userFullname);
-                parameters.Add("permissions", "read_stream");
-                parameters.Add("method", "post");
-                parameters.Add("access_token", accessToken);
+            parameters.Add("installed", "true");
+            parameters.Add("name", userFullname);
+            parameters.Add("permissions", "read_stream");
+            parameters.Add("method", "post");
+            parameters.Add("access_token", accessToken);
 
-                string url = "https://graph.facebook.com/" + applicationId + "/accounts/test-users?" +
-                                       parameters.AsQuerystring();
+            string url = "https://graph.facebook.com/" + applicationId + "/accounts/test-users?" +
+                                    parameters.AsQuerystring();
                 
-                var response = webClient.DownloadString(new Uri(url));
+            var response = DownloadString(new Uri(url));
 
-                return JsonConvert.DeserializeObject<APITestUser>(response);
-            }
+            return JsonConvert.DeserializeObject<APITestUser>(response);
         }
 
         public class APIUserList
@@ -75,37 +70,44 @@ namespace FBDerp
         /// <returns></returns>
         public static IEnumerable<APITestUser> GetSomeExistingTestUsers(string accessToken, string applicationId)
         {
-            using (var webClient = new WebClient())
-            {
-                var parameters = new QuerystringParameters();
+            var parameters = new QuerystringParameters();
 
-                parameters.Add("access_token", accessToken);
+            parameters.Add("access_token", accessToken);
 
-                string url = "https://graph.facebook.com/" + applicationId + "/accounts/test-users?" +
-                                       parameters.AsQuerystring();
+            string url = "https://graph.facebook.com/" + applicationId + "/accounts/test-users?" +
+                                    parameters.AsQuerystring();
 
-                var response = webClient.DownloadString(new Uri(url));
+            var response = DownloadString(new Uri(url));
 
-                Console.WriteLine(response);
-                return JsonConvert.DeserializeObject<APIUserList>(response).data;
-            }
+            Console.WriteLine(response);
+            return JsonConvert.DeserializeObject<APIUserList>(response).data;
         }
 
         public static void DeleteUser(string accessToken, string userId)
         {
-            using (var webClient = new WebClient())
+            var parameters = new QuerystringParameters();
+
+            parameters.Add("access_token", accessToken);
+
+            string createUserUri = "https://graph.facebook.com/" + userId +"?" +
+                                    parameters.AsQuerystring();
+
+            var response = DownloadString(new Uri(createUserUri), "DELETE");
+
+            if (response != "true")
+                throw new Exception("Unable to delete user " + userId + ", response was: " + response);
+        }
+
+        private static string DownloadString(Uri url, string method = "GET")
+        {
+            return DownloadString(url.AbsoluteUri, method);
+        }
+
+        private static string DownloadString(string url, string method = "GET")
+        {
+            using (var client = new WebClient())
             {
-                var parameters = new QuerystringParameters();
-
-                parameters.Add("access_token", accessToken);
-
-                string createUserUri = "https://graph.facebook.com/" + userId +"?" +
-                                       parameters.AsQuerystring();
-
-                var response = webClient.UploadString(new Uri(createUserUri), "DELETE");
-
-                if (response != "true")
-                    throw new Exception("Unable to delete user " + userId + ", response was: " + response);
+                return client.UploadString(url, method);
             }
         }
     }
