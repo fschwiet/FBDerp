39de9dc361fa8c2a50a0e48414dd472fee6f0bca
fschwiet <fschwiet@gmail.com>
2011-12-23 17:53:50 -0800

    reset db on test

--- FBDerp/TestDrivers/IISExpressTestExtensions.cs
+++ FBDerp/TestDrivers/IISExpressTestExtensions.cs
@@ -8,9 +8,21 @@ namespace FBDerp.TestDrivers
     {
         public static IISExpressDriver ArrangeServer(this GivenWhenThenFixture fixture)
         {
-            var serverUnderTest = fixture.beforeAll(() => new IISExpressDriver());
-
             var sitePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Properties.Settings.Default.SimpleServerPath);
+
+            fixture.beforeAll(delegate()
+            {
+                var appDataPath = Path.Combine(sitePath, "App_Data");
+                foreach(string file in Directory.GetFiles(appDataPath, "*"))
+                {
+                    File.Delete(file);
+
+                    if (File.Exists(file))
+                        throw new Exception("unable to delete file " + file);
+                }
+            });
+
+            var serverUnderTest = fixture.beforeAll(() => new IISExpressDriver());
 
             sitePath = Path.GetFullPath(sitePath);
 
